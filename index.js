76f714a5ee00b10ef3f4ab4d4363434050913d3c
Simon Bothén <simon.bothen@radinn.com>
2022-06-01 16:56:41 +0200

    Add clean-build input
    Allow the option to supply whenever the build should be
    clean or not.

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function run() {
     );
     process.env.BUILD_SDK = core.getInput("build-sdk");
     process.env.BUILD_DESTINATION = core.getInput("build-destination");
+    process.env.CLEAN_BUILD = core.getInput("clean-build");
 
     // Execute build.sh
     await exec.exec(`bash ${__dirname}/../build.sh`);
